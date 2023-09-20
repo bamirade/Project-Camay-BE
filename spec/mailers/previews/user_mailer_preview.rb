bbf1949bc8bf540b613732c7891fb620b882ee96
bamirade <118993260+bamirade@users.noreply.github.com>
2023-09-20 22:06:24 +0800

    Test Setup and CI Pipeline

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def confirmation_email
+    user = FactoryBot.build(:user)
+    UserMailer.confirmation_email(user)
+  end
 
+  def approval_email
+    user = FactoryBot.build(:user)
+    UserMailer.approval_email(user)
+  end
 end
---
+++ spec/models/seller_spec.rb
@@ -0,0 +1,23 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
+require 'rails_helper'
+
+RSpec.describe Seller, type: :model do
+  it "has a valid factory" do
+    seller = create(:seller)
+    expect(seller).to be_valid
+  end
+
+  it "belongs to a user" do
+    seller = build(:seller, user: nil)
+    expect(seller).not_to be_valid
+  end
+
+  it "sets default values for bio, portfolio, and seller_rating" do
+    seller = Seller.new # Create a new seller instance without using FactoryBot
+    expect(seller.bio).to eq('')
+    expect(seller.portfolio).to eq('')
+    expect(seller.seller_rating).to eq(0.0)
+  end
+end
